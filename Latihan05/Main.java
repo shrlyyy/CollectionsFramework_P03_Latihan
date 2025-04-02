cdbc736fd50d2224151b6af75e6a34e61e5b92d8
Sherly <162469801+shrlyyy@users.noreply.github.com>
2025-04-02 21:52:11 +0700

    Add files via upload

---
+++ Latihan05/Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        list.add("Jenie");
+        list.add("Sherly");
+        list.add("Nopeliba");
+        list.add("Oktria");
+        list.add("Angelyne");
+        list.add("Michella");
+        System.out.println("2 : " + list.get(2));
+        System.out.println("0: " + list.get(0));
+
+        LinkedList queue = new LinkedList();
+        queue.addFirst("Oktria");
+        queue.addFirst("Nopeliba");
+        queue.addFirst("Jenie");
+        queue.addFirst("Michella");
+        queue.addFirst("Sherly");
+        queue.addFirst("Angelyne");
+        System.out.println(queue);
+
+        queue.removeLast();
+        queue.removeLast();
+        System.out.println(queue);
+    }
+}
